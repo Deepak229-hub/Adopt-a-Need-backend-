613c366dc74760092caa3d3d2112ed6f3f0ac6ab
Deepak Tiwari <tiwarideepak.dk.2004@gmail.com>
2026-01-07 17:34:43 +0530

    modified for deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import pool from "./database/pg.js";
 import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors({
     origin: "http://localhost:5173",
@@ -17,6 +17,6 @@ app.get("/", (req, res) => {
     res.status(200).json({msg: "Welcome"});
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`server is listening at port ${PORT}`);
 });
