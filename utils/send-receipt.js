ec91c760412b9ddc9a9a8de3c5eea38f53937a98
Deepak Tiwari <tiwarideepak.dk.2004@gmail.com>
2026-01-15 13:56:11 +0530

    used another email service

--- utils/send-receipt.js
+++ utils/send-receipt.js
@@ -1,30 +1,25 @@
-import nodemailer from "nodemailer";
+import { Resend } from 'resend';
 
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL_ID,
-        pass: process.env.EMAIL_PASS,
-    },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function sendReciept(payment) {
-    const html = `
-        <h2>Thank you for your donation ❤️</h2>
-        <p><b>Name: </b>${payment.name}</p>
-        <p><b>Email: </b>${payment.email}</p>
-        <p><b>Amount: </b>${payment.amount}</p>
-        <p><b>Transaction ID: </b>${payment.payment_id}</p>
-        <p><b>Domain: </b>${payment.domain}</p>
-        <p><b>Date: </b>${new Date(payment.date).toLocaleString("en-IN")}</p>
-        <hr />
-        <p>Adopt-a-Need Foundation</p>
-    `;
-
-    await transporter.sendMail({
-        from: `"Adopt-a-Need" <${process.env.EMAIL_ID}>`,
+    const result = await resend.emails.send({
+        from: "onboarding@resend.dev",
         to: payment.email,
         subject: "Your Donation Receipt",
-        html,
+        html: `
+            <h2>Thank you for your donation ❤️</h2>
+            <p><b>Name: </b>${payment.name}</p>
+            <p><b>Email: </b>${payment.email}</p>
+            <p><b>Amount: </b>${payment.amount}</p>
+            <p><b>Transaction ID: </b>${payment.payment_id}</p>
+            <p><b>Domain: </b>${payment.domain}</p>
+            <p><b>Date: </b>${new Date(payment.date).toLocaleString("en-IN")}</p>
+            <hr />
+            <p>Adopt-a-Need Foundation</p>
+        `
     });
+
+    console.log(result);
+    
 };
